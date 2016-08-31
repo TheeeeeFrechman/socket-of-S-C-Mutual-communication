4c4767ce9060a91ab1555478072b1bd7536821d7
Eric King <598020642@qq.com>
2016-08-31 23:57:06 +0800

    Update client.c

--- client/client.c
+++ client/client.c
@@ -24,28 +24,32 @@ int init_client_socket(int sockfd,int argc, char **argv)
     	struct sockaddr_in s_addr;
 
     	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-        	{
-                	perror("socket");
-                	return -1;
-        	}else
+       	{
+                perror("socket");
+                return -1;
+        }
+        else
+        {
         	printf("socket create success!\n");
-
-    		memset(&s_addr,0,sizeof(s_addr));
-    		s_addr.sin_family = AF_INET;
-     		s_addr.sin_port = htons(PORT);
-    		if (inet_aton(argv[1], (struct in_addr *)&s_addr.sin_addr.s_addr) == 0)
-	 		{
-               	 		perror(argv[1]);
-                		return -1;
-        		}
-    		if(connect(sockfd,(struct sockaddr*)&s_addr,sizeof(struct sockaddr)) == -1)
-        		{
-                		perror("connect");
-                		return -1;
-        		}else
-        			printf("conncet success!\n");
-				return sockfd;	
+        }
+    	memset(&s_addr,0,sizeof(s_addr));
+    	s_addr.sin_family = AF_INET;
+     	s_addr.sin_port = htons(PORT);
+    	if (inet_aton(argv[1], (struct in_addr *)&s_addr.sin_addr.s_addr) == 0)
+	{
+        	perror(argv[1]);
+                return -1;
+       	}
+    	if(connect(sockfd,(struct sockaddr*)&s_addr,sizeof(struct sockaddr)) == -1)
+        {
+        	perror("connect");
+                return -1;
+        }
+        else
+        	printf("conncet success!\n");
+	return sockfd;	
 }
+
 int client_RecvSend(int sockfd)
 {
 	while(1)
@@ -60,93 +64,99 @@ int client_RecvSend(int sockfd)
                 tv.tv_usec = 0;
                 retval = select(maxfd+1, &rfds, NULL, NULL, &tv);
                 if(retval == -1)
-        {
-            printf("select wrong,client quit.\n");
-            break;
-        }else if(retval == 0)
+        	{
+            		printf("select wrong,client quit.\n");
+            		break;
+        	}
+        	else if(retval == 0)
                 {
-                printf("waiting...\n");
-                continue;
-                }else
-                        {
-		if(FD_ISSET(sockfd,&rfds))
-                        {
-                        bzero(buf,BUFLEN);
-                        //len = recv(sockfd,buf,BUFLEN,0);
-                        len = recv_fun(sockfd,buf,BUFLEN,0);
-                if(len > 0)
-                        {
-                    printf("server message：%s\n",buf);
-                        }
-                else{
-                    if(len < 0 )
-                        printf("message fail to recv！\n");
-                    else
-                        printf("server quit！\n");
-                break;
+                	printf("waiting...\n");
+                	continue;
                 }
-            }
-            if(FD_ISSET(0, &rfds))
+                else
                 {
-                        bzero(buf,BUFLEN);
-                        fgets(buf,BUFLEN,stdin);
-		 if(!strncasecmp(buf,"quit",4))
+			if(FD_ISSET(sockfd,&rfds))
                         {
-                                printf("client quit!\n");
-                                break;
-                        }
-                    //len = send(sockfd,buf,strlen(buf),0);
-                    len = send_fun(sockfd,buf,strlen(buf),0);
-                if(len > 0)
-                    printf("\tmessage：%s\n",buf);
-                else{
-                    printf("message!\n");
-                    break;
-                }
-            }
-        }
+                        	bzero(buf,BUFLEN);
+                        	//len = recv(sockfd,buf,BUFLEN,0);
+                        	len = recv_fun(sockfd,buf,BUFLEN,0);
+                		if(len > 0)
+                        	{
+                    			printf("server message：%s\n",buf);
+                        	}
+                		else
+                		{
+                    			if(len < 0 )
+                        			printf("message fail to recv！\n");
+                    			else
+                        			printf("server quit！\n");
+                			break;
+                		}
+            		}
+	            	if(FD_ISSET(0, &rfds))
+	                {
+	                        bzero(buf,BUFLEN);
+	                        fgets(buf,BUFLEN,stdin);
+				if(!strncasecmp(buf,"quit",4))
+	                        {
+	                                printf("client quit!\n");
+	                                break;
+	                        }
+	                    	//len = send(sockfd,buf,strlen(buf),0);
+	                    	len = send_fun(sockfd,buf,strlen(buf),0);
+	                	if(len > 0)
+	                    		printf("\tmessage：%s\n",buf);
+	                	else
+	                	{
+	                    		printf("message!\n");
+	                    		break;
+	                	}
+	            	}
+        	}
 
-    }
+    	}
 	close(sockfd);
 	return 0;
 }
+
 int recv_fun(int connfd,char *buffer,int bufferLength, int option)
 {
         int value_max = 1;
         int ReBytes = 0;
         do
-                {       
-                        if ((value_max= recv(connfd, (buffer + ReBytes), bufferLength, option)) == 0)
+        {       
+                if ((value_max= recv(connfd, (buffer + ReBytes), bufferLength, option)) == 0)
+                {
+                        printf("client closed.\n");
+                        close(connfd);
+                        return -1;
+                }
+                else
+                {
+                        if (value_max != ReBytes)
                         {
-                                printf("client closed.\n");
-                                close(connfd);
-                                return -1;
+                                ReBytes += value_max;
+                                continue;
                         }
-                        else
-                        {
-                                if (value_max != ReBytes)
-                                {
-                                        ReBytes += value_max;
-                                        continue;
-                                }
-                        }
-                }while (value_max != ReBytes);
+                }
+        }while (value_max != ReBytes);
                 
-                buffer[value_max] = '\0';
+        buffer[value_max] = '\0';
         return value_max;
 }
+
 int send_fun(int connfd,char *buffer,int bufferLength,int option)
 {
         int returnvalus = 0;
         int The_max = 0;
         while(bufferLength > The_max)
         {
-        returnvalus = send(connfd, buffer, bufferLength, option);
-        The_max += returnvalus;
-        if(returnvalus < 0)
+	        returnvalus = send(connfd, buffer, bufferLength, option);
+	        The_max += returnvalus;
+	        if(returnvalus < 0)
                 {
-                printf("server send fail! \n");
-                return -1;
+	                printf("server send fail! \n");
+	                return -1;
                 }
         }
         return 1;
